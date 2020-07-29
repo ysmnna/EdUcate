ebc707fd97aa4beca54ebdc1bcb8d9dbf423221b
Yasmin Jama <yjama102@gmail.com>
2020-07-29 11:28:30 -0700

    first push

---
+++ EdUcate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EdUcate
+//
+//  Created by Yasmin Jama on 7/29/20.
+//  Copyright © 2020 EYKR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
